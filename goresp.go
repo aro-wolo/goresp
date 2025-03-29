2b146b2d35144c102995e34b65e1abfd65de0493
Arowolo Abiodun <aarowolom@gmail.com>
2025-03-29 17:41:36 +0100

    code refactor

--- goresp.go
+++ goresp.go
@@ -20,156 +20,73 @@ func New(c *gin.Context) *Responder {
 	return &Responder{ctx: c}
 }
 
-func (r *Responder) Ok(data interface{}, messages ...string) {
-	message := ""
-
+func (r *Responder) sendJSON(status int, defaultMsg string, isError bool, data interface{}, messages ...string) {
+	message := defaultMsg
 	if len(messages) > 0 {
 		message = messages[0]
 	}
 
-	r.ctx.JSON(http.StatusOK, DataResponse{
-		Message: message,
-		Data:    data,
-		IsError: false,
-	})
-}
-
-func (r *Responder) Created(data interface{}, messages ...string) {
-	message := "Resource created successfully"
-
-	if len(messages) > 0 {
-		message = messages[0]
-	}
-
-	r.ctx.JSON(http.StatusCreated, DataResponse{
-		Message: message,
-		Data:    data,
-		IsError: false,
-	})
-}
-
-func (r *Responder) ServerError(messages ...string) {
-	message := "Internal Server Error" // Default message
-
-	if len(messages) > 0 {
-		message = messages[0]
-	}
-
-	r.ctx.JSON(http.StatusInternalServerError, DataResponse{
-		Message: message,
-		IsError: true,
-	})
-}
-
-func (r *Responder) BadRequest(messages ...string) {
-	message := "Bad Request" // Default message
-
-	if len(messages) > 0 {
-		message = messages[0]
-	}
-
-	r.ctx.JSON(http.StatusBadRequest, DataResponse{
-		Message: message,
-		IsError: true,
-	})
-}
-
-func (r *Responder) Error404(messages ...string) {
-	message := "Not Found" // Default message
-
-	if len(messages) > 0 {
-		message = messages[0]
-	}
-
-	r.ctx.JSON(http.StatusNotFound, DataResponse{
-		Message: message,
-		IsError: true,
-	})
-}
-
-func (r *Responder) AccessDenied(messages ...string) {
-	message := "Access denied"
-
-	if len(messages) > 0 {
-		message = messages[0]
-	}
-
-	r.ctx.JSON(http.StatusUnauthorized, DataResponse{
-		Message: message,
-		IsError: true,
-	})
-
-	r.ctx.Abort()
-}
-
-func (r *Responder) Forbidden(messages ...string) {
-	message := "Forbidden"
-
-	if len(messages) > 0 {
-		message = messages[0]
-	}
-
-	r.ctx.JSON(http.StatusForbidden, DataResponse{
-		Message: message,
-		IsError: true,
-	})
-}
-
-func (r *Responder) Conflict(messages ...string) {
-	message := "Conflict" // Default message
-
-	if len(messages) > 0 {
-		message = messages[0]
-	}
-
-	r.ctx.JSON(http.StatusConflict, DataResponse{
-		Message: message,
-		IsError: true,
-	})
-}
-
-func (r *Responder) UnprocessableEntity(messages ...string) {
-	message := "Unprocessable Entity" // Default message
-
-	if len(messages) > 0 {
-		message = messages[0]
-	}
-
-	r.ctx.JSON(http.StatusUnprocessableEntity, DataResponse{
-		Message: message,
-		IsError: true,
-	})
-}
-
-func (r *Responder) TooManyRequests(messages ...string) {
-	message := "Too Many Requests" // Default message
-
-	if len(messages) > 0 {
-		message = messages[0]
-	}
-
-	r.ctx.JSON(http.StatusTooManyRequests, DataResponse{
-		Message: message,
-		IsError: true,
-	})
-}
-
-func (r *Responder) JSON(code int, message string, data interface{}, isError bool) {
-	r.ctx.JSON(code, DataResponse{
+	r.ctx.JSON(status, DataResponse{
 		Message: message,
 		Data:    data,
 		IsError: isError,
 	})
 }
 
+// Success responses
+func (r *Responder) Ok(data interface{}, messages ...string) {
+	r.sendJSON(http.StatusOK, "Success", false, data, messages...)
+}
+
+func (r *Responder) Created(data interface{}, messages ...string) {
+	r.sendJSON(http.StatusCreated, "Resource created successfully", false, data, messages...)
+}
+
+// Error responses
+func (r *Responder) BadRequest(messages ...string) {
+	r.sendJSON(http.StatusBadRequest, "Bad Request", true, nil, messages...)
+}
+
+func (r *Responder) UnprocessableEntity(messages ...string) {
+	r.sendJSON(http.StatusUnprocessableEntity, "Unprocessable Entity", true, nil, messages...)
+}
+
+func (r *Responder) ServerError(messages ...string) {
+	r.sendJSON(http.StatusInternalServerError, "Internal Server Error", true, nil, messages...)
+}
+
+func (r *Responder) Error404(messages ...string) {
+	r.sendJSON(http.StatusNotFound, "Not Found", true, nil, messages...)
+}
+
+func (r *Responder) Forbidden(messages ...string) {
+	r.sendJSON(http.StatusForbidden, "Forbidden", true, nil, messages...)
+}
+
+func (r *Responder) Conflict(messages ...string) {
+	r.sendJSON(http.StatusConflict, "Conflict", true, nil, messages...)
+}
+
+func (r *Responder) TooManyRequests(messages ...string) {
+	r.sendJSON(http.StatusTooManyRequests, "Too Many Requests", true, nil, messages...)
+}
+
+func (r *Responder) AccessDenied(messages ...string) {
+	r.sendJSON(http.StatusUnauthorized, "Access Denied", true, nil, messages...)
+	r.ctx.Abort()
+}
+
+// JSON response with custom status code
+func (r *Responder) JSON(code int, message string, data interface{}, isError bool) {
+	r.sendJSON(code, message, isError, data)
+}
+
 func (r *Responder) ShouldBind(obj interface{}, customErr ...string) bool {
 	if err := r.ctx.ShouldBindJSON(obj); err != nil {
 		message := "Invalid request payload: " + err.Error()
-
 		if len(customErr) > 0 {
 			message = customErr[0]
 		}
-
 		r.BadRequest(message)
 		return false
 	}
